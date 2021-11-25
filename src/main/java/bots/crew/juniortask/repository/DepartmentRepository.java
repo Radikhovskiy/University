f98c0c0375f4b0a3673eaaeefa49252bfa6ad404
Roman Radikhovskiy <radihovskiyroman@gmail.com>
2021-11-25 22:53:20 +0200

    Implemented BotsCrew task

--- src/main/java/bots/crew/juniortask/repository/DepartmentRepository.java
+++ src/main/java/bots/crew/juniortask/repository/DepartmentRepository.java
@@ -1,9 +1,13 @@
 package bots.crew.juniortask.repository;
 
 import bots.crew.juniortask.entity.Department;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    @Query("from Department d JOIN FETCH d.lectors")
+    List<Department> findAll();
 }
